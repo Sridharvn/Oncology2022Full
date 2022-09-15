d4d327b1369b19e4049bdb85272780f8a23c037f
Sridharvn <sridharvnampoothiripad@gmail.com>
2022-09-15 23:11:03 +0530

    Added css properties to BusAnnouncement

--- src/components/BusAnnouncement.vue
+++ src/components/BusAnnouncement.vue
@@ -2,13 +2,19 @@
   <div class="announcement">
     <h1>Announcement</h1>
     <div class="announcement-container">
-      <v-img src="./../assets/Megaphone-removebg-preview.png"></v-img>
+      <v-img
+        src="./../assets/Megaphone-removebg-preview.png"
+        class="image-container megaphone"
+      ></v-img>
       <v-spacer></v-spacer>
       <h2 class="text-color-class">
         There will be luxury Bus Trips at 4:30PM from Hotel Le Meridien to Trivandrum via
         Alappuzha and to Calicut via Thrissur on 16/10/2022
       </h2>
-      <v-img src="./../assets/Bus-removebg-preview.png"></v-img>
+      <v-img
+        src="./../assets/Bus-removebg-preview.png"
+        class="image-container bus"
+      ></v-img>
     </div>
   </div>
 </template>
@@ -22,5 +28,22 @@
 }
 .announcement {
   background-color: var(--primary-color);
+}
+.image-container {
+  height: fit-content;
+  width: fit-content;
+}
+@media screen and (max-width: 750px) {
+  .announcement-container {
+    flex-direction: column;
+    // justify-content: center;
+    align-items: center;
+    .image-container {
+      display: none;
+    }
+    .bus {
+      display: block;
+    }
+  }
 }
 </style>
