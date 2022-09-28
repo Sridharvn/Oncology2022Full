db25ab0083c25d0ef248e4cfdf3654c9567279e4
Sridharvn <sridharvnampoothiripad@gmail.com>
2022-09-29 00:19:51 +0530

    Edited 14th october schedule

--- src/components/Schedule/ScheduleFor14thOct2022.vue
+++ src/components/Schedule/ScheduleFor14thOct2022.vue
@@ -1,18 +1,41 @@
 <template>
-  <!-- <div class="schedule-container">
-    <v-img src="./../../assets/schedule/Schedule14th1.png" max-width="1100px"></v-img>
-    <v-img src="./../../assets/schedule/Schedule14th2.png" max-width="1100px"></v-img>
-    <v-img src="./../../assets/schedule/Schedule14th3.png" max-width="1100px"></v-img>
-  </div> -->
   <div>
     <h1>14th October 2022 (Day 1) - Hall A (Oman Hall)</h1>
     <h2 class="left text-color-class">Registration - 7:30 AM Onwards</h2>
     <v-card class="mx-auto">
       <v-timeline align-top dense>
         <v-timeline-item v-for="schedule in scheduleList" :key="schedule" small>
+          <div class="time">{{ schedule.time }}</div>
+          <br /><br />
           {{ schedule.Description }}
-          <br />
-          {{ schedule.Speaker }}
+          <br /><br />
+          <div class="speaker">{{ schedule.Speaker }}</div>
+          <hr />
+        </v-timeline-item>
+      </v-timeline>
+    </v-card>
+    <h1>14th October 2022 (Day 1) - Hall B</h1>
+    <v-card class="mx-auto">
+      <v-timeline align-top dense>
+        <v-timeline-item v-for="schedule in scheduleListHallB" :key="schedule" small>
+          <div class="time">{{ schedule.time }}</div>
+          <br /><br />
+          {{ schedule.Description }}
+          <br /><br />
+          <div class="speaker">{{ schedule.Speaker }}</div>
+          <hr />
+        </v-timeline-item>
+      </v-timeline>
+    </v-card>
+    <h1>14th October 2022 (Day 1) - Hall C</h1>
+    <v-card class="mx-auto">
+      <v-timeline align-top dense>
+        <v-timeline-item v-for="schedule in scheduleListHallC" :key="schedule" small>
+          <div class="time">{{ schedule.time }}</div>
+          <br /><br />
+          {{ schedule.Description }}
+          <br /><br />
+          <div class="speaker">{{ schedule.Speaker }}</div>
           <hr />
         </v-timeline-item>
       </v-timeline>
@@ -43,26 +66,36 @@ export default {
     return {
       scheduleList: [
         {
+          time: "8:00-8:20",
+          Description: "Small cell lung cancer: Management of extensive stage disease",
+          Speaker: "Dr. Grace Dy MD, FACP",
+        },
+        {
+          time: "8:25-8:45",
           Description:
             "Molecular Oncology in the early diagnosis and prevention of Oral Cancer",
           Speaker: "Dr. Amritha Suresh MD",
         },
         {
+          time: "8:50-9:10",
           Description:
             "Immunotherapy in recurrent and locally advanced Head and Carcinomas - The current scenario",
           Speaker: "Dr. Cessal T. Kainickal MD, DNB,MRCP",
         },
         {
+          time: "9:15-9:35",
           Description:
             "Do we require concomitant chemotherapy in the era of the IGRT in Nasiopharygeal cancer",
           Speaker: "Dr. Sarbani GhoshLaskar MD, DNB",
         },
         {
+          time: "9:40-10:00",
           Description:
             "Advances in improvement of quality of life and survival in GE Junction Carcinomas",
           Speaker: "Dr. Kurian Joseph FRCR,FRCP",
         },
         {
+          time: "10:05-10:25",
           Description:
             "Updates in the management of metastatic prostate cancer (CSPC and CRPC)",
           Speaker: "Dr. Saby George MD, FACP",
@@ -72,121 +105,225 @@ export default {
         //   Speaker: "Dr. Khaleel Ashraf MD,FACP",
         // },
         {
+          time: "10:30-10:50",
           Description: "NACT on Oral Cavity Cancer - Does it benefit Indian patients",
           Speaker: "Dr. Kumar Prabhash MD,DM",
         },
         {
+          time: "10:55-11:15",
           Description: "Recent advances in the management of type 1 Endometrial cancer",
           Speaker: "Dr. Umesh Mahant Shetty MD",
         },
         {
+          time: "11:20-11:40",
           Description: "Type II Endometrial Cancer — Current trends ",
           Speaker: "Prof. Dr.Shashikant B. Lele MD, FACOG",
         },
+
         {
-          Description: "Improving outcomes in Cervical cancer ",
+          time: "11:45-12:05",
+          Description: "Current concepts in the management of type 1 Endometrial cancer",
           Speaker: "Dr. Umesh Mahant Shetty MD",
         },
-        // {
-        //   Description: "Current concepts in the management of type 1 Endometrial cancer",
-        //   Speaker: "Dr. Umesh Mahant Shetty MD",
-        // },
         {
+          time: "12:10-12:30",
           Description:
             "Maximizing outcomes in Pre-Menopausal Patients with HR+/HER2- Metastatic Breast Cancer",
           Speaker: "Dr. Ajith Veniyoor MD, DM",
         },
         {
+          time: "12:35-12:55",
           Description:
             "Management of brain metastasis in the era of Targeted therapy & Immunotherapy",
           Speaker: " Dr. Anil. K. Anand MD",
         },
         {
+          time: "13:00-13:20",
           Description: "Interstitial Brachytherapy in Ca Cervix",
           Speaker: "Dr. Nisar M Syed MD, FRCS",
         },
+        { time: "13:25-14:15", Description: "Lunch Break", Speaker: "" },
         {
-          Description: "Lunch Break",
-          Speaker: "",
-        },
-        {
+          time: "14:15-14:35",
           Description:
             "Colo-rectal cancer management: Current standards and emerging option",
           Speaker: "Speaker: Dr. Patrick Boland MD, FACP",
         },
+        // {
+        //   Description:
+        //     "Brachytherapy techniques and local control in Head & Neck cancer- Plenary session",
+        //   Speaker: "Dr. Nisar M Syed MD, FRCS",
+        // },
         {
-          Description:
-            "Brachytherapy techniques and local control in Head & Neck cancer- Plenary session",
-          Speaker: "Dr. Nisar M Syed MD, FRCS",
-        },
-        {
+          time: "14:40-15:00",
           Description: "Proton Therapy for Paediatric solid tumours-The evidence",
           Speaker: "Dr. Siddhartha Laskar MD, DNB",
         },
         {
+          time: "15:05-15:25",
           Description:
             "Molecular profiling in lung cancer —Does it benefit Indian patients.",
           Speaker: "Dr. Kumar Prabhash MD, DM",
         },
         {
+          time: "15:30-15:50",
           Description: "Post-operative management of Carcinoma of urinary bladder",
           Speaker: "Dr. Kannan MD",
         },
         {
+          time: "15:55-16:15",
           Description: "Evolving treatment landscape in Chronic Lymphatic Leukemia",
           Speaker: "Dr. Linu Jacob MD, DM",
         },
         {
-          Description: "Approach to biochemical recurrences after Radical Prostatectomy",
-          Speaker: "Dr. Francis V James MD",
-        },
-        {
-          Description:
-            "T1G3 TCC: Is there a case for early Cystectomy - Panel Discussion",
-          Speaker: "Dr. Kim J Mammen MS, Mch, FRCS",
-        },
-        {
-          Description:
-            "Advances in the management Prostate Cancer Oligo Metastasis- Panel discussion ",
-          Speaker: "To Be Finalised",
-        },
-        {
-          Description: "Management of recurrent Head and Neck Cancer - Panel Discussion",
-          Speaker: "Moderator: Dr. Cessal Kainickal MD",
-        },
-        {
-          Description: "Case based Panel Discussion in Lung cancer To Be Finalised",
-          Speaker: "Dr. Sanju Cyriac MD,DM",
-        },
-        {
-          Description: `Cross-sectional anatomy for Oncologists Course on Radiology of Oncology (8 hours) and target delineation would be of great value for all practicing Oncologists especially Radiation Oncologists and Physicists, who are involved in the care of cancer patients. As we all know Dr. Kanhu Charan Pathro is the most hardworking Radiation Oncologist in the country and he is bent on teaching students and oncologists the intricacies in radiological anatomy and target volume delineation. May we request all to participate in this highly interactive course to become more confident in improving therapeutic ratio. The modules of one day refresher course in Radiology of Oncology for Oncologists and Oncology Residents is as follows`,
-          Speaker: "Dr. K.C Pathro MD,DNB,MBA",
-        },
-        {
-          Description: `Salient features 
-          1. Basics
-          2. Identification
-          3. CT-MRI correlation
-          4. Target delineation correlation
-          5. Common pathologies
-          Chapters covered
-          1. Basics of basic investigations- 1 hr
-          2. Radiology of brain- 1.5 hr
-          3. Radiology of skull base- 1 hr
-          4. Radiology of neck- 1.5 hr
-          5. Radiology of thorax- 1.5 hr
-          6. Radiology of abdomen and pelvis - 1.5 hr
-          TOTAL DURATION OF THE COURSE - 8 HOURS`,
+          time: "16:20-16:40",
+          Description: "TBD",
           Speaker: "",
         },
         {
-          Description: `Prof. Manoj Gupta MD, DNB, who is shortly becoming the next President of AROI is well known to all of us. He  will be doing the course over 8 hours and it will be very valuable for Radiation Oncology Residents, Physicists and Radiation Oncologists to master the tough subject “Clinical Radiobiology”`,
-          Speaker: "",
+          time: "16:45-17:05",
+          Description: "Improving outcomes in Cervical cancer ",
+          Speaker: "Dr. Umesh Mahant Shetty MD",
         },
+        {
+          time: "17:10-17:40",
+          Description:
+            "Molecular Guided Therapeutic Options (MGTO) in Precision Oncology",
+          Speaker: "Dr. Amit Verma MD",
+        },
+        // {
+        //   Description: "Approach to biochemical recurrences after Radical Prostatectomy",
+        //   Speaker: "Dr. Francis V James MD",
+        // },
+        // {
+        //   Description:
+        //     "T1G3 TCC: Is there a case for early Cystectomy - Panel Discussion",
+        //   Speaker: "Dr. Kim J Mammen MS, Mch, FRCS",
+        // },
+        // {
+        //   Description:
+        //     "Advances in the management Prostate Cancer Oligo Metastasis- Panel discussion ",
+        //   Speaker: "To Be Finalised",
+        // },
+        // {
+        //   Description: "Management of recurrent Head and Neck Cancer - Panel Discussion",
+        //   Speaker: "Moderator: Dr. Cessal Kainickal MD",
+        // },
+        // {
+        //   Description: "Case based Panel Discussion in Lung cancer To Be Finalised",
+        //   Speaker: "Dr. Sanju Cyriac MD,DM",
+        // },
+        // {
+        //   Description: `Cross-sectional anatomy for Oncologists Course on Radiology of Oncology (8 hours) and target delineation would be of great value for all practicing Oncologists especially Radiation Oncologists and Physicists, who are involved in the care of cancer patients. As we all know Dr. Kanhu Charan Pathro is the most hardworking Radiation Oncologist in the country and he is bent on teaching students and oncologists the intricacies in radiological anatomy and target volume delineation. May we request all to participate in this highly interactive course to become more confident in improving therapeutic ratio. The modules of one day refresher course in Radiology of Oncology for Oncologists and Oncology Residents is as follows`,
+        //   Speaker: "Dr. K.C Pathro MD,DNB,MBA",
+        // },
+        // {
+        //   Description: `Salient features
+        //   1. Basics
+        //   2. Identification
+        //   3. CT-MRI correlation
+        //   4. Target delineation correlation
+        //   5. Common pathologies
+        //   Chapters covered
+        //   1. Basics of basic investigations- 1 hr
+        //   2. Radiology of brain- 1.5 hr
+        //   3. Radiology of skull base- 1 hr
+        //   4. Radiology of neck- 1.5 hr
+        //   5. Radiology of thorax- 1.5 hr
+        //   6. Radiology of abdomen and pelvis - 1.5 hr
+        //   TOTAL DURATION OF THE COURSE - 8 HOURS`,
+        //   Speaker: "",
+        // },
+        // {
+        //   Description: `Prof. Manoj Gupta MD, DNB, who is shortly becoming the next President of AROI is well known to all of us. He  will be doing the course over 8 hours and it will be very valuable for Radiation Oncology Residents, Physicists and Radiation Oncologists to master the tough subject “Clinical Radiobiology”`,
+        //   Speaker: "",
+        // },
         // {
         //   Description: "",
         //   Speaker: "",
         // },
+      ],
+      scheduleListHallB: [
+        {
+          time: "14:15-14:30",
+          Description:
+            "Management of high-grade muscle invasive bladder cancer - Medical oncology perspective",
+          Speaker: "Dr. Ashish Singh MD,DM",
+        },
+        {
+          time: "14:35-14:50",
+          Description:
+            "Bladder preservation strategies in the era of new development in Radiotherapy",
+          Speaker: "Dr. Sreenivas Chilukuri MD",
+        },
+        {
+          time: "15:20-15:40",
+          Description: "Panel Discussion on Ca Bladder",
+          Speaker: `Moderator: 
+Panelists:-
+    1. Dr. Srivinvas Chilikuri MD
+    2. Dr. Arun Philip MD DM
+    3. Dr. Ashwin kumar MD
+    4. Dr. Ashish Singh MD,DM
+    5. Dr. Harish`,
+        },
+        {
+          time: "15:45-16:45",
+          Description: "Management of recurrent Head and Neck Cancer- Panel Discussion",
+          Speaker: `Moderator: Dr. Surij Salih MS MCh and Dr. Cessal Thomas Kainickal
+MD,DNB,MRCP
+Panelistss:     1. Dr. Mathew Jose MD,DM
+    2. Dr. Shaji Thomas MS,Mch
+    3. Dr. Jomon Raphel MD
+    4. Dr. Ajayakumar MD DNB 
+    5. Dr. Madhu Sairam MD
+    6. Dr. Rajanish MD`,
+        },
+        {
+          time: "16:50-17:50",
+          Description: "Case based Panel discussion on Ca Lung",
+          Speaker: `Moderator: - Dr. Sanju Cyriac MD,DM
+
+Panelists:-
+    1. Dr. Ashok Komaranchath MD, DM, MRCP, FRCP
+    2. Dr. Sivanandan MD
+    3. Dr. Dinesh Makuny MD DNB
+    4. Dr. Varun Rajan  MD DM
+    5. Dr. Durgapoorna MD
+    6. Dr. Bharath Rangarajan MD, DM
+`,
+        },
+      ],
+      scheduleListHallC: [
+        {
+          time: "09:00-13:00",
+          Description: `Cross-sectional anatomy for Oncologists
+
+Course on Radiology of Oncology (8 hours) and target delineation would be of great value for all practicing Oncologists especially Radiation Oncologists and Physicists, who are involved in the care of cancer patients. As we all know Dr. Kanhu Charan Pathro is the most hardworking Radiation Oncologist in the country and he is bent on teaching students and oncologists the intricacies in radiological anatomy and target volume delineation. May we request all to participate in this highly interactive course to become more confident in improving therapeutic ratio.
+
+The modules of one day refresher course in Radiology of Oncology for Oncologists and Oncology Residents is as follows
+Salient features
+    1. Basics
+    2. Identification
+    3. CT-MRI correlation
+    4. Target delineation correlation
+    5. Common pathologies
+Chapters covered
+    1. Basics of basic investigations- 1 hr
+    2. Radiology of brain- 1.5 hr
+    3. Radiology of skull base- 1 hr
+    4. Radiology of neck- 1.5 hr
+    5. Radiology of thorax- 1.5 hr
+    6. Radiology of abdomen and pelvis- 1.5 hr
+TOTAL DURATION OF THE COURSE- 8 HOURS
+”.`,
+          Speaker: "Dr. K.C Pathro MD,DNB,MBA",
+        },
+        {
+          time: "",
+          Description: `Prof. Manoj Gupta MD, DNB, who is shortly becoming the next President of AROI is well known to all of us. He will be doing the course over 8 hours and it will be very valuable for Radiation Oncology Residents, Physicists and Radiation Oncologists to master the tough subject “Clinical Radiobiology`,
+          Speaker: "",
+        },
       ],
     };
   },
@@ -247,7 +384,12 @@ export default {
 .text2-color-class {
   color: var(--link-color) !important;
 }
-
+.time {
+  color: var(--link-color);
+}
+.speaker {
+  color: var(--TabSlider-color);
+}
 td,
 th {
   // set width to 33%
