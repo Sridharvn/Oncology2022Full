913acdac6e2879bcb3fb8120a128026a8f7f699f
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-03-22 18:00:34 +0530

    Added gallery images

---
+++ src/components/GalleryImages.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="gallery">
+    <div class="gallery__row">
+      <div class="gallery__col" v-for="client in images" :key="client.pathShort">
+        <v-img class="gallery__image" :src="client.pathLong" width="20vw"></v-img>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.gallery {
+  &__col {
+    margin-bottom: 30px;
+    -webkit-box-shadow: 0px 12px 17px 7px rgba(0, 0, 0, 0.53);
+    -moz-box-shadow: 0px 12px 17px 7px rgba(0, 0, 0, 0.53);
+    box-shadow: 0px 12px 17px 7px rgba(0, 0, 0, 0.53);
+  }
+
+  &__image {
+    margin: 5px;
+    margin-bottom: 30px;
+    -webkit-box-shadow: inset 0px 0px 137px 12px rgba(0, 0, 0, 1);
+    -moz-box-shadow: inset 0px 0px 137px 12px rgba(0, 0, 0, 1);
+    box-shadow: inset 0px 0px 137px 12px rgba(0, 0, 0, 1);
+  }
+  &__row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+}
+</style>
+<script>
+export default {
+  name: "SectionOurClients",
+  data() {
+    return {
+      images: [],
+    };
+  },
+
+  mounted() {
+    this.importAll(require.context("../../dist/img/Gallery", true, /\.png$/));
+  },
+
+  methods: {
+    importAll(r) {
+      r.keys().forEach((key) => this.images.push({ pathLong: r(key), pathShort: key }));
+    },
+  },
+};
+</script>
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>Hi</div>
+  <div class="gallery">
+    <h1 class="gallery__heading">Photos from the Event</h1>
+
+    <GalleryImages></GalleryImages>
+  </div>
 </template>
+<style lang="scss" scoped>
+.gallery {
+  &__heading {
+    margin-bottom: 40px;
+  }
+}
+</style>
+<script>
+import GalleryImages from "../components/GalleryImages.vue";
+
+export default {
+  components: { GalleryImages },
+  data() {
+    return {};
+  },
+};
+</script>
